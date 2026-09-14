3d8092e5596f814b709676295808d106f0780166
yanlan <43209899+saaassin13@users.noreply.github.com>
2026-09-14 14:36:07 +0800

    Route scheduled task page

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import DashboardView from './views/DashboardView.vue'
+import SchedulesView from './views/SchedulesView.vue'
 import TaskView from './views/TaskView.vue'
 
 export default createRouter({
   history: createWebHashHistory(),
   routes: [
     { path: '/', name: 'dashboard', component: DashboardView },
+    { path: '/schedules', name: 'schedules', component: SchedulesView },
     { path: '/tasks/:id', name: 'task', component: TaskView },
   ],
 })
